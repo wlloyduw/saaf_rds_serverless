0dc33256e67228ef1ee09a765b7c93047a13d3d6
wlloyduw <wlloyd@uw.edu>
2024-11-08 22:04:45 -0800

    Update callservice.sh
    added cli-binary-format option for aws cli 2
    switched to us-east-1
    removed old line with call to specific function
    removed {}'s around function name

--- java_template/test/callservice.sh
+++ java_template/test/callservice.sh
@@ -1,3 +1,4 @@
+/7
 #!/bin/bash
 
 # JSON object to pass to Lambda Function
@@ -13,8 +14,7 @@ json={"\"name\"":"\"Susan\u0020Smith\",\"param1\"":1,\"param2\"":2,\"param3\"":3
 #echo ""
 
 echo "Invoking Lambda function using AWS CLI"
-#time output=`aws lambda invoke --invocation-type RequestResponse --function-name {LAMBDA-FUNCTION-NAME} --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
-time output=`aws lambda invoke --invocation-type RequestResponse --function-name hellomysqlf21 --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+time output=`aws lambda invoke --invocation-type RequestResponse --cli-binary-format raw-in-base64-out --function-name LAMBDA-FUNCTION-NAME --region us-east-1 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
 
 echo ""
 echo "JSON RESULT:"
